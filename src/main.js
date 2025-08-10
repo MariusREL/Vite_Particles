2c3bd86b9332246233c57d0297067e39de1d18d2
Marius R <Mrel0425@gmail.com>
2025-08-10 13:59:01 +0200

    refactor: clean up comments and simplify click handler in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import "./style.css";
 import { tsParticles } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 
-// wait DOM ready and load the full v2 bundle so shapes/interactions in options are registered
 document.addEventListener("DOMContentLoaded", async () => {
   await loadFull(tsParticles);
   await tsParticles.load({
@@ -521,15 +520,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     },
   });
 
-  tsParticles.setOnClickHandler((event, particles) => {
-    /* custom on click handler */
-  });
+  tsParticles.setOnClickHandler((event, particles) => {});
 
-  // now you can control the animations too, it's possible to pause and resume the animations
-  // these methods don't change the config so you're safe with all your configurations
-  // domItem(0) returns the first tsParticles instance loaded in the dom
   const particles = tsParticles.domItem(0);
-  // play will start the animations, if the move is not enabled it won't enable it, it just updates the frame
+
   particles?.play();
-  // don't auto-pause on init
 });
